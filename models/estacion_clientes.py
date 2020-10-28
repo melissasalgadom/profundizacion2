0d552eac2740905f5b56d0719eaa996842c697e8
melissasalgadom <melissasalgadomejia@gmail.com>
2020-10-27 20:49:30 -0700

    ajustes en clientes y eventos

--- models/estacion_clientes.py
+++ models/estacion_clientes.py
@@ -14,7 +14,7 @@ class Clientes(models.Model):
      ('3', 'Cedula de extranjeria'),
      ('4', 'Pasaporte'),
     ], string='Tipo de Documento', required=True, index=True, track_visibility='onchange', track_sequence=3, default="1")
-    documento = fields.Char(string='Serial', required=True, size=150)
+    documento = fields.Char(string='Documento', required=True, size=150)
     correo_electronico = fields.Char(string='Correo electronico', required=True, size=150)
     fecha_nacimiento = fields.Date(string='Fecha Nacimiento', required=True)
     _sql_constraints = {('cobrador_uniq', 'unique(documento)', 'El numero de documento debe ser unico')}
--- models/estacion_eventos.py
+++ models/estacion_eventos.py
@@ -9,6 +9,8 @@ class Estacion(models.Model):
     _order = 'name asc'
     name = fields.Char(string='Nombre evento', required=True, size=150)
     pluviosidad = fields.Char(string='Pluviosidad', required=True, size=150)
+    sensor1 = fields.Char(string='Sensor 1', required=True, size=150)
+    sensor2 = fields.Char(string='Sensor 2', required=True, size=150)
     fecha_creacion = fields.Date(string='Fecha Creacion', required=True)
     estacion_id = fields.Many2one('estacion.estacion', 'Estacion', required=True)
    #_sql_constraints = {('cobrador_uniq', 'unique(documento)', 'El numero de documento debe ser unico')}
