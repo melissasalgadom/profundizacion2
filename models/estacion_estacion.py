775b0066870201aefdb603b87f56c77e309d0251
melissasalgadom <melissasalgadomejia@gmail.com>
2020-11-07 09:21:36 -0800

    comunicacion con la raspberry

--- models/estacion_estacion.py
+++ models/estacion_estacion.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
+import json
+import requests
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class Estacion(models.Model):
     _name = 'estacion.estacion' # exaflow_documento
@@ -14,3 +18,26 @@ class Estacion(models.Model):
     fecha_instalacion = fields.Date(string='Fecha Instalacion', required=True)
     cliente_id = fields.Many2one('estacion.clientes', 'Cliente', required=True)
     _sql_constraints = {('cobrador_uniq', 'unique(serial)', 'El serial debe ser unico')}
+    blacklist = fields.Boolean(string="Encender", default=False)
+
+    @api.onchange('blacklist')
+    def onchange_blacklist(self):
+        _logger.info('Entro al blacklist')
+        if self.blacklist == True:
+            dato = 1
+        else:
+            dato = 0
+
+        headers = {
+            'Authorization': 'Basic b2RvbzoxNjQzMTU2OTUzNTE2ODQ1MzUxNjg0NTY4NA==',
+            'Content-Type': 'application/json'
+        }
+        #data = {'enable': dato}
+        url = 'https://home.juxn3.com:8100'
+        #datajson = json.dumps(data)
+        #_logger.info(datajson)
+        payload = {"enable": dato}
+        response = requests.request("POST", url, headers=headers, data=payload)
+        _logger.info(response)
+
+
